a1dfe54b834909ec48a2a83f9a6a457bd50670e3
Ryan Chandler <ryan@chandler.codes>
2020-03-28 18:12:24 +0000

    Forelse for empty state

--- source/index.blade.php
+++ source/index.blade.php
@@ -25,7 +25,7 @@
             <h2 class="text-lg text-gray-700 mb-8">Articles</h2>
             <table class="w-full">
                 <tbody>
-                @foreach($posts as $post)
+                @forelse($posts as $post)
                     <tr>
                         <td class="p-0 align-middles">
                             <a href="{{ $post->getUrl() }}" class="underline" style="text-decoration-color: #718096;">{{ $post->title }}</a>
@@ -34,7 +34,9 @@
                             {{ date('j, M Y', $post->date) }}
                         </td>
                     </tr>
-                @endforeach
+                @empty
+                    <p>🤦🏻‍♂️ Don't worry, something will be here soon!</p>
+                @endforelse
                 </tbody>
             </table>
         </section>
