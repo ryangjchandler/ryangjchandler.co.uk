7662d411b73a645a85298a3d449fc41e3bb98fce
Ryan Chandler <ryan@chandler.codes>
2020-03-28 21:04:38 +0000

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,6 +6,9 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>@yield('title')</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet"
+      href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
     </head>
     <body class="antialiased font-sans max-w-2xl mx-auto px-8 md:px-0 pb-12 md:pb-16">
         @include('_partials/_header')
