e0572baf8f6db055d94d98dfa011f90fcbb66e58
Ryan Chandler <ryan@chandler.codes>
2020-03-31 18:14:41 +0100

    Limit index to show 3 latest articles

--- source/index.blade.php
+++ source/index.blade.php
@@ -35,7 +35,7 @@
         </section>
         <section>
             <h2 class="text-lg text-gray-700 mb-8">Articles</h2>
-            @forelse($posts as $post) 
+            @forelse($posts->take(3) as $post) 
                 @include('_partials._post-card')
 
                 @if(! $loop->last)
