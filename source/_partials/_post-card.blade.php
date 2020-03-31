e0da71f6acf2aab9977fe4902198c02aaa70c02b
Ryan Chandler <ryan@chandler.codes>
2020-03-31 15:52:16 +0100

    Switched title and date around on post card

--- source/_partials/_post-card.blade.php
+++ source/_partials/_post-card.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col mb-8">
-    <time datetime="{{ date('Y-m-d', $post->date) }}" class="text-gray-700 text-sm md:text-base mb-3">{{ date('j, M Y', $post->date) }}</time>
-    <h2 class="font-semibold md:text-lg mb-4">
+    <h2 class="font-semibold md:text-lg mb-3">
         <a href="{{ $post->getUrl() }}">{{ $post->title }}</a>
     </h2>
+    <time datetime="{{ date('Y-m-d', $post->date) }}" class="text-gray-700 text-sm md:text-base mb-4">{{ date('j, M Y', $post->date) }}</time>
     @include('_partials._categories')
 </div>
