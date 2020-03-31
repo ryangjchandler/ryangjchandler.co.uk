28b5806287620204bbd86480a6dcc11340a5c083
Ryan Chandler <ryan@chandler.codes>
2020-03-31 18:11:20 +0100

    Nicer desktop footer styling & remove .env....

--- source/_partials/_footer.blade.php
+++ source/_partials/_footer.blade.php
@@ -1,7 +1,10 @@
-<footer class="text-sm text-gray-600 pl-0 md:pl-4 mt-8 border-t pt-4 pb-8 md:pb-4">
-    <div class="mb-4">
+<footer class="flex items-center justify-between flex-col-reverse md:flex-row text-sm text-gray-600 pl-0 mt-8 border-t pt-4 pb-12">
+    <div>
+        &copy; 2020 Ryan Chandler All rights reserved.
+    </div>
+    <div class="mb-4 md:mb-0">
         <nav>
-            <ul class="flex items-center justify-center">
+            <ul class="flex items-center">
                 <li class="mr-4">
                     <a href="/timeline">Timeline</a>
                 </li>
@@ -16,8 +19,5 @@
                 </li>
             </ul>
         </nav>
-    </div>
-    <div class="text-center">
-        &copy; 2020 Ryan Chandler All rights reserved.
     </div>
 </footer>
