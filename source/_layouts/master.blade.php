bf48d20bb428567b8b26eb92b9ef518eede45226
Ryan Chandler <ryan@chandler.codes>
2020-03-31 19:45:29 +0100

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>@yield('title')</title>
+        <link rel="webmention" href="https://webmention.io/ryangjchandler.co.uk/webmention" />
+        <link rel="pingback" href="https://webmention.io/ryangjchandler.co.uk/xmlrpc" />
         <link href="https://fonts.googleapis.com/css?family=Inter:400,600,700,800&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Fira+Code:500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
