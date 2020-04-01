86031d9066a36e584283d9f9f91dcc2710628a4b
Ryan Chandler <ryan.c@surewise.com>
2020-04-01 13:06:46 +0100

    Add leading zeros to dates

--- source/_partials/_post-card.blade.php
+++ source/_partials/_post-card.blade.php
@@ -2,7 +2,7 @@
     <h2 class="font-semibold md:text-lg mb-3">
         <a href="{{ $post->getUrl() }}">{{ $post->title }}</a>
     </h2>
-    <time datetime="{{ date('Y-m-d', $post->date) }}" class="text-gray-700 text-sm md:text-base mb-4">{{ date('j, M Y', $post->date) }}</time>
+    <time datetime="{{ date('Y-m-d', $post->date) }}" class="text-gray-700 text-sm md:text-base mb-4">{{ date('d, M Y', $post->date) }}</time>
     <p class="mb-4">{{ $post->excerpt(150) }}</p>
     @include('_partials._categories')
 </div>
