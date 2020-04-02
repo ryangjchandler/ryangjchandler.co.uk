d233c735e96072751a78b5fe47733ec972b9a900
Ryan Chandler <ryan@chandler.codes>
2020-04-02 14:09:26 +0100

    Markdown parser changes done

--- app/Markdown/Parser.php
+++ app/Markdown/Parser.php
@@ -4,14 +4,40 @@ namespace RyanChandler\Markdown;
 
 use TightenCo\Jigsaw\Parsers\MarkdownParser;
 use GitDown\GitDown;
+use League\CommonMark\Block\Element\FencedCode;
+use League\CommonMark\Block\Element\IndentedCode;
+use League\CommonMark\CommonMarkConverter;
+use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
+use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
+use League\CommonMark\Environment;
+use TightenCo\Jigsaw\Parsers\JigsawMarkdownParser;
 
 class Parser extends MarkdownParser
 {
+    protected $commonmark;
+
+    protected static $languages = [
+        'html', 'php', 'js', 'css', 'yaml', 'bash',
+    ];
+
+    public function __construct(JigsawMarkdownParser $parser = null)
+    {
+        parent::__construct();
+
+        $this->commonmark = static::createCommonmark();
+    }
+
     public function parse($markdown)
     {
-        return (new GitDown(
-            env('GITHUB_TOKEN'),
-            'ryangjchandler/ryangjchandler'
-        ))->parse($markdown);
+        return $this->commonmark->convertToHtml($markdown);
+    }
+
+    protected static function createCommonmark()
+    {
+        $environment = Environment::createCommonMarkEnvironment()
+            ->addBlockRenderer(FencedCode::class, new FencedCodeRenderer(static::$languages))
+            ->addBlockRenderer(IndentedCode::class, new IndentedCodeRenderer(static::$languages));
+
+        return new CommonMarkConverter([], $environment);
     }
 }
