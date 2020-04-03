f6b1b97d3d5ca0520ef659fa8a832499dca1e1bd
Ryan Chandler <ryan@chandler.codes>
2020-04-03 15:57:02 +0100

    Removed version number from footer

--- source/_partials/_footer.blade.php
+++ source/_partials/_footer.blade.php
@@ -14,12 +14,6 @@
                 <li class="ml-4">
                     <a href="mailto:contact@ryangjchandler.co.uk">Email</a>
                 </li>
-                <li class="ml-4" x-data="{ showVersion: false }">
-                    <a href="#" x-show="!showVersion" @click.prevent="showVersion = !showVersion">Version</a>
-                    <a href="#" x-show="showVersion" @click.prevent="showVersion = !showVersion" class="font-mono">
-                        {{ $page->getVersion() }}
-                    </a>
-                </li>
             </ul>
         </nav>
     </div>
