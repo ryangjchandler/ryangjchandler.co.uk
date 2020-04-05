a50cbe66c9f484748a46d15c5943974fc0c3f296
Ryan Chandler <ryan@chandler.codes>
2020-04-05 19:22:10 +0100

    Revert "Moved favicons into resources/favicons"
    This reverts commit f266967040dbe12a7f1a264d6eb4a9865bfea8c3.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,10 +9,10 @@
         <meta property="og:locale" content="en_GB">
         <meta property="og:description" content="Ryan Chandler is a Laravel developer and wannabe writer.">
         <meta property="og:url" content="https://ryangjchandler.co.uk">
-	    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
-	    <link rel="manifest" href="/favicons/site.webmanifest">
+	    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	    <link rel="manifest" href="/site.webmanifest">
         <script type='application/ld+json'>
             {
                 "@context":"http:\/\/schema.org",
