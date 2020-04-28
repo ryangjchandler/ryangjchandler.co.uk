30214f02ca82016a73a2de5b115f068e53c5876d
Ryan Chandler <ryan@chandler.codes>
2020-04-28 16:57:08 +0100

    Titles are good for me.

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,3 +1,9 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link type="text/plain" rel="author" href="{{ url('/humans.txt') }}">
+
+@isset($title)
+    <title>{{ $title }} - Ryan Chandler</title>
+@else
+    <title>Ryan Chandler</title>
+@endisset
