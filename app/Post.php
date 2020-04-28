7fb97f3ee1b7e60c9e18db8c52e8c57a5d4da148
Ryan Chandler <ryan@chandler.codes>
2020-04-28 17:33:18 +0100

    Setup events for Post model

--- app/Post.php
+++ app/Post.php
@@ -3,10 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     protected $guarded = [];
+
+    public static function booted()
+    {
+        static::creating(function (Post $post) {
+            if (! $post->slug) {
+                $post->slug = Str::slug($post->title);
+            }
+
+            if ($post->published) {
+                $post->published_at = now();
+            }
+        });
+
+        static::saving(function (Post $post) {
+            if ($post->published && ! $post->published_at) {
+                $post->published_at = now();
+            }
+        });
+    }
 
     public function categories()
     {
