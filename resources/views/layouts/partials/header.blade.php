237b2ddf430a34658f901e9303d26f92537c2c26
Ryan Chandler <ryan@chandler.codes>
2020-04-29 14:55:51 +0100

    Underline links

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,9 +3,9 @@
         <a href="#">Ryan Chandler</a>
     </h1>
     <nav>
-        <a href="{{ route('articles.index') }}" style="margin-right: 10px;">Articles</a>
-        <a href="{{ route('categories.index') }}" style="margin-right: 10px;">Categories</a>
-        <a href="{{ route('contact') }}" style="margin-right: 10px;">Contact</a>
-        <a href="{{ route('feed') }}">RSS</a>
+        <a href="{{ route('articles.index') }}" style="text-decoration: underline; margin-right: 10px;">Articles</a>
+        <a href="{{ route('categories.index') }}" style="text-decoration: underline; margin-right: 10px;">Categories</a>
+        <a href="{{ route('contact') }}" style="text-decoration: underline; margin-right: 10px;">Contact</a>
+        <a href="{{ route('feed') }}" style="text-decoration: underline;">RSS</a>
     </nav>
 </header>
