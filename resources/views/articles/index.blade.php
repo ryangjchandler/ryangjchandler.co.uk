38f0c19de6b4a0d64ef5eee3548adb1726b3255e
Ryan Chandler <ryan@chandler.codes>
2020-06-30 21:45:19 +0100

    wip

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,7 +7,7 @@
         <h2 class="text-2xl font-bold mb-4">Articles</h2>
         <p class="md:text-lg text-gray-700">Or more accurately, word dumps.</p>
     </section>
-    <section class="flex">
+    <section class="flex flex-col md:flex-row">
         <div class="w-3/4">
             @forelse($articles as $article)
                 @include('partials.article-card')
@@ -15,10 +15,10 @@
                 <p class="font-medium text-gray-700">Nothing to see here. 😐</p>
             @endforelse
         </div>
-        <aside class="flex flex-col align-end w-1/4">
-            <h3 class="text-lg font-bold text-right text-gray-700 hover:text-gray-900">Archive</h3>
+        <aside class="flex flex-col mt-24 w-1/4 | md:mt-0">
+            <h3 class="text-lg font-bold md:text-right text-gray-700 hover:text-gray-900">Archive</h3>
             @foreach($dates as $date => $items)
-                <a href="?date={{ $date }}" class="text-right text-primary-400 hover:text-primary-600 hover:underline font-medium mt-4">
+                <a href="?date={{ $date }}" class="md:text-right text-primary-400 hover:text-primary-600 hover:underline font-medium mt-4">
                     {{ $date }} ({{ $items->count() }})
                 </a>
             @endforeach
