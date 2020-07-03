941dbe61fba1f3476403b83fa94f3b68391e91c0
Ryan Chandler <ryan@chandler.codes>
2020-07-03 16:58:54 +0100

    wip

--- app/Models/Commit.php
+++ app/Models/Commit.php
@@ -15,7 +15,11 @@ class Commit
             return collect(
                 Http::get('https://api.github.com/repos/ryangjchandler/ryangjchandler.co.uk/commits')->json()
             )->last();
-        })[0];
+        });
+
+        if ($commit) {
+            $commit = ['', ''];
+        }
 
         return [Str::limit($commit['sha'], 7), $commit['commit']['url']];
     }
