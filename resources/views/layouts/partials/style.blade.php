0e74d917b442213549040565a4de365bfef8ae2e
Ryan Chandler <ryan@chandler.codes>
2020-07-13 14:58:59 +0100

    feature(fathom analytics)

--- resources/views/layouts/partials/style.blade.php
+++ resources/views/layouts/partials/style.blade.php
@@ -1,3 +1,9 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 @stack('style')
+
+@if(app()->environment('production'))
+    <!-- Fathom - beautiful, simple website analytics -->
+    <script src="https://shrew.ryangjchandler.co.uk/script.js" site="FSVTNSHG" defer></script>
+    <!-- / Fathom -->
+@endif
