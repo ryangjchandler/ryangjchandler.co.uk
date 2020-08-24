43a1b88f5572a7dac05cdd7db374a8d16e748d28
Ryan Chandler <ryan@chandler.codes>
2020-08-24 15:37:21 +0100

    wip

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,3 @@
-import { registerComponents } from './utils.js'
-
 ;(function () {
-    registerComponents({
-
-    })
-
     import('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js')
 })()
