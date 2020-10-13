39ddd914799886c1fe9c2b5f8b019ea2522d3042
Ryan Chandler <ryan@chandler.codes>
2020-10-13 13:04:11 +0100

    fix(tests): make series_id column nullable

---
+++ database/migrations/2020_10_13_120329_make_series_id_column_nullable_on_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeSeriesIdColumnNullableOnArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->unsignedBigInteger('series_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
