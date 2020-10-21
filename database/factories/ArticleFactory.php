08eac1ef2df73e38b68f8db608b7e5af76e6376e
Ryan Chandler <ryan@chandler.codes>
2020-10-21 12:06:38 +0100

    tests(preview articles): fix factories

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Article;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use PDO;
 
 class ArticleFactory extends Factory
 {
@@ -16,12 +17,20 @@ class ArticleFactory extends Factory
         ]);
     }
 
+    public function published()
+    {
+        return $this->state([
+            'published_at' => now()->subDay(),
+        ]);
+    }
+
     public function definition()
     {
         return [
             'title' => $this->faker->text(10),
             'content' => $this->faker->text(200),
             'excerpt' => $this->faker->text(50),
+            'published_at' => now()->subDay(),
         ];
     }
 }
