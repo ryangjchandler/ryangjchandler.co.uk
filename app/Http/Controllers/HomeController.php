65c61c68403a99129bb92a8f7fed6686e390a184
Ryan Chandler <ryan@chandler.codes>
2020-10-29 14:17:31 +0000

    feature: 500

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ class HomeController
 {
     public function __invoke()
     {
+        abort(500);
         return view('pages.home', [
             'articles' => Article::latest('published_at')
                 ->published()
