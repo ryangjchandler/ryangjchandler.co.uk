b2696987518d631204dd70655ec34081a6f587fb
Ryan Chandler <ryangjchandler@gmail.com>
2021-01-25 23:56:29 +0000

    Update style.blade.php

--- resources/views/layouts/partials/style.blade.php
+++ resources/views/layouts/partials/style.blade.php
@@ -6,4 +6,8 @@
     <!-- Fathom - beautiful, simple website analytics -->
     <script src="https://shrew.ryangjchandler.co.uk/script.js" site="FSVTNSHG" defer></script>
     <!-- / Fathom -->
+    
+    <!-- Umami - self-hosted, privacy-focused analytics -->
+    <script async defer data-website-id="c79f67c1-bdef-433c-81f5-d9c1149a616f" src="https://analytics.ryangjchandler.co.uk/umami.js"></script>
+    <!-- / Umami -->
 @endif
