9f6aa749ff403c19a266a99c7e65eb63e686eafe
Ryan Chandler <ryangjchandler@gmail.com>
2021-01-26 11:37:48 +0000

    chore: install breeze

--- tailwind.config.js
+++ tailwind.config.js
@@ -854,5 +854,7 @@ module.exports = {
         wordBreak: ['responsive'],
         zIndex: ['responsive', 'focus-within', 'focus'],
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 }
