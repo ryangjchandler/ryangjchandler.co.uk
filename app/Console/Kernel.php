c3cfbd174a57e604b27155581d7e6bc94674a5d2
Ryan Chandler <ryangjchandler@gmail.com>
2021-01-26 11:58:23 +0000

    wip [skip-ci]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,10 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        Commands\GenerateSitemap::class,
-    ];
+    protected $commands = [];
 
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(Commands\GenerateSitemap::class)->dailyAt('22:00');
+        //
     }
 }
