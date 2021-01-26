e89a4a2ddda192fed852fcbfcaba68da9b0329ef
Ryan Chandler <ryangjchandler@gmail.com>
2021-01-26 11:50:52 +0000

    wip [skip-ci]

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,5 +1,13 @@
 <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+    <div>
+        {{ $logo }}
+    </div>
+
     <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
         {{ $slot }}
     </div>
+
+    @isset($after)
+        {{ $after }}
+    @endisset
 </div>
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'shadow-sm border-gray-300 focus:border-gray-700 focus:ring focus:ring-black focus:ring-opacity-75']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'shadow-sm border-gray-300 focus:ring-offset-2 focus:border-gray-700 focus:ring focus:ring-black focus:ring-opacity-75']) !!}>
